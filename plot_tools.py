25ba5659a611b75b9a4cf3caa44a528daa376f3a
Shahid Khan <47216877+shahidzk1@users.noreply.github.com>
2021-03-15 15:00:40 +0100

    Add files via upload

--- plot_tools.py
+++ plot_tools.py
@@ -32,6 +32,9 @@ def AMS(y_true, y_predict, y_true1, y_predict1):
     plt.xlim([-0.01, 1.0])
     plt.ylim([0, 1.02])
     #axs.axis([-0.01, 1, 0.9, 1])
+
+    return S0_best_threshold, S0_best_threshold1
+
     fig.tight_layout()
     plt.show()
 
@@ -47,3 +50,34 @@ def preds_prob(df, preds, true):
     plt.xlabel('Probability')
     plt.legend()
     plt.show()
+
+import itertools
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label',fontsize = 15)
+    plt.xlabel('Predicted label',fontsize = 15)
