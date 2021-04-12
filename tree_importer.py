214a95c650b66732dee2c8c3f079f55e260b5d69
Shahid Khan <47216877+shahidzk1@users.noreply.github.com>
2021-04-12 16:34:26 +0200

    Update tree_importer.py

--- tree_importer.py
+++ tree_importer.py
@@ -1,6 +1,11 @@
+import uproot
+from concurrent.futures import ThreadPoolExecutor
+"""
+The tree_importer function takes in flat analysis tree and returns a pandas data-frame object. The executor variable accepts the number of parallel processors
+available.
+"""
+
 def tree_importer(path,treename):
-    import uproot
-    from concurrent.futures import ThreadPoolExecutor
     import pandas as pd
     executor = ThreadPoolExecutor(8)
     file = uproot.open(path+':'+treename+'', library='pd', decompression_executor=executor,
