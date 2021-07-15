7dc181a1a45c11a2f6c398fcdd19750438d6146b
Shahid Khan <47216877+shahidzk1@users.noreply.github.com>
2021-07-15 15:15:08 +0200

    Add files via upload

--- tree_importer.py
+++ tree_importer.py
@@ -3,8 +3,7 @@ from concurrent.futures import ThreadPoolExecutor
 import numpy as np
 import pandas as pd
 """
-The tree_importer function takes in flat analysis tree and returns a pandas data-frame object. The executor variable accepts the number of parallel processors
-available.
+The tree_importer function takes in flat analysis tree and returns a pandas data-frame object. It has 3 inputs, the first one is path of the analysis tree, the second one as the tree name and third one the number of CPU cores. The first and second input should be inserted as strings i.e. path inside a single quotation '' or double quotations "". The third input should be a number. For example  tree_importer("/home/flat_trees/a.tree","PlainTree",4)
 """
 
 def tree_importer(path,treename, n):
@@ -14,7 +13,7 @@ def tree_importer(path,treename, n):
     new_labels=['chi2geo', 'chi2primneg','chi2primpos', 'distance', 'ldl','mass', 'pT', 'rapidity','issignal']
 
     executor = ThreadPoolExecutor(n)
-    file = uproot.open(path+':'+treename+'', library='pd', decompression_executor=executor,
+    file = uproot.open(path+':'+str(treename)+'', library='pd', decompression_executor=executor,
                                   interpretation_executor=executor).arrays(labels,"(LambdaCandidates_mass < 1.3) & (LambdaCandidates_mass > 1.07) & (LambdaCandidates_pz>0) & (LambdaCandidates_p<20)  & (LambdaCandidates_distance>0) & (LambdaCandidates_distance<100) & (LambdaCandidates_chi2geo>0) & (LambdaCandidates_chi2geo<1000) & (LambdaCandidates_chi2topo>0) & (LambdaCandidates_chi2primpos<1e6) & (LambdaCandidates_chi2primneg < 3e7) & (LambdaCandidates_ldl>0) & (LambdaCandidates_ldl<5000) & (LambdaCandidates_chi2topo < 100000) & (LambdaCandidates_cosineneg>0.1) & (LambdaCandidates_cosinepos>0.1) & (LambdaCandidates_eta>1) & (LambdaCandidates_eta<6.5) & (LambdaCandidates_l<80) & (LambdaCandidates_x>-50) & (LambdaCandidates_x<50) & (LambdaCandidates_y>-50) & (LambdaCandidates_y<50) & (LambdaCandidates_z>-1) & (LambdaCandidates_z<80)", library='np',decompression_executor=executor,
                                   interpretation_executor=executor)
     df= pd.DataFrame(data=file)
