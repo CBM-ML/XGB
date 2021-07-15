9dfa9b1f18eddbcfe769146182e50f8cfdddb0d9
Shahid Khan <47216877+shahidzk1@users.noreply.github.com>
2021-07-15 16:10:16 +0200

    Add files via upload

--- tree_importer.py
+++ tree_importer.py
@@ -5,7 +5,7 @@ import pandas as pd
 """
 The tree_importer function takes in flat analysis tree and returns a pandas data-frame object. It has 3 inputs, the first one is
 path of the analysis tree, the second one as the tree name and third one the number of CPU cores. The first and second input
-should be inserted as strings i.e. path inside a single quotation '' or double quotations "". The third input should be a number.
+should be inserted as strings i.e. path inside a single quotation '' or double quotations "". The third input should be a number. This function also applies quality selection criteria on the data along with some further data reducing cuts.
 For example  tree_importer("/home/flat_trees/a.tree","PlainTree",4)
 """
 
@@ -16,7 +16,23 @@ def tree_importer(path,treename, n):
 
     new_labels=['chi2geo', 'chi2primneg','chi2primpos', 'distance', 'ldl','mass', 'pT', 'rapidity','issignal']
     
-    cuts="(LambdaCandidates_mass < 1.3) & (LambdaCandidates_mass > 1.07) & (LambdaCandidates_pz>0) & (LambdaCandidates_p<20)  & (LambdaCandidates_distance>0) & (LambdaCandidates_distance<100) & (LambdaCandidates_chi2geo>0) & (LambdaCandidates_chi2geo<1000) & (LambdaCandidates_chi2topo>0) & (LambdaCandidates_chi2primpos<1e6) & (LambdaCandidates_chi2primneg < 3e7) & (LambdaCandidates_ldl>0) & (LambdaCandidates_ldl<5000) & (LambdaCandidates_chi2topo < 100000) & (LambdaCandidates_cosineneg>0.1) & (LambdaCandidates_cosinepos>0.1) & (LambdaCandidates_eta>1) & (LambdaCandidates_eta<6.5) & (LambdaCandidates_l<80) & (LambdaCandidates_x>-50) & (LambdaCandidates_x<50) & (LambdaCandidates_y>-50) & (LambdaCandidates_y<50) & (LambdaCandidates_z>-1) & (LambdaCandidates_z<80)"
+    mass_cut = "(LambdaCandidates_mass < 1.3) & (LambdaCandidates_mass > 1.07) &"
+    
+    coordinate_cut = "(LambdaCandidates_x>-50) & (LambdaCandidates_x<50) & (LambdaCandidates_y>-50) & (LambdaCandidates_y<50) & (LambdaCandidates_z>-1) & (LambdaCandidates_z<80) &"
+    
+    chi_2_positive_cut ="(LambdaCandidates_chi2geo>0) & (LambdaCandidates_chi2topo>0) & (LambdaCandidates_chi2primpos>0) & (LambdaCandidates_chi2primneg > 0) &"
+    
+    distance_cut = "(LambdaCandidates_ldl>0) & (LambdaCandidates_l<80) & (LambdaCandidates_distance>0) & (LambdaCandidates_distance<100) &"
+    
+    pz_cut = "(LambdaCandidates_pz>0) & "
+    
+    pseudo_rapidity_cut_based_on_acceptance = "(LambdaCandidates_eta>1) & (LambdaCandidates_eta<6.5) &"
+    
+    angular_cut = "(LambdaCandidates_cosineneg>0.1) & (LambdaCandidates_cosinepos>0.1) &"
+    
+    data_reducing_cut = " (LambdaCandidates_p<20)  &   (LambdaCandidates_chi2geo<1000) &  (LambdaCandidates_chi2primpos<1e6) & (LambdaCandidates_chi2primneg < 3e7) &  (LambdaCandidates_ldl<5000) & (LambdaCandidates_chi2topo < 100000)"
+    
+    cuts= mass_cut+coordinate_cut+chi_2_positive_cut+distance_cut+pz_cut+pseudo_rapidity_cut_based_on_acceptance+angular_cut+data_reducing_cut
 
     executor = ThreadPoolExecutor(n)
     file = uproot.open(path+':'+str(treename)+'', library='pd', decompression_executor=executor,
