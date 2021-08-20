3287e0f26ca8a1e760d6f06d0f19e84d31622130
conformist89 <olhalavoryk@gmail.com>
2021-08-20 11:06:09 +0200

    Invoked the distributions in the main file

--- clusterXGB.py
+++ clusterXGB.py
@@ -316,11 +316,7 @@ plot1Dcorrelation(vars_to_draw_corr,'mass', corr_signal, corr_signal_errors,
 
 
 pdf_2d_mass_feat = PdfPages(output_path+'/'+'2d_mass_feat_train.pdf')
-# for var in vars_to_draw_corr:
-#     plot2D(dfs_orig_train, 'mass', var, [1.108, 1.1227], [dfs_orig_train[var].min(),
-#     dfs_orig_train[var].min()], pdf_2d_mass_feat)
-#
-# pdf_2d_mass_feat.close()
+
 
 
 def dist2D(df, vars,x_range, pdf):
@@ -343,13 +339,10 @@ def dist2D(df, vars,x_range, pdf):
         if var in non_log_x:
             new_log_x.append(var)
 
-    print(df_new.columns)
-    print("DF: ", df_new)
-    print("new log_x: ", new_log_x)
 
     for feat in new_log_x:
 
-        plot2D(df_new, 1, 'mass', feat, x_range,[df_new[feat].min(),
+        plot2D(df_new, ' train',  1, 'mass', feat, x_range,[df_new[feat].min(),
         df_new[feat].max()], pdf)
 
 
@@ -402,7 +395,12 @@ pdf_cuts_deploy = PdfPages(output_path+'/'+'dist_cuts_urqmd_deploy.pdf')
 
 cut_visualization(deploy_data,'issignalXGB',test_best, output_path)
 
-def variables_distribution(dfs_orig, dfb_orig, dfs_cut, dfb_cut, cuts_plot, pdf_cuts):
+def variables_distribution(sample, dfs_orig, dfb_orig, dfs_cut, dfb_cut, cuts_plot, pdf_cuts):
+    # dfs_orig_new = dfs_orig.copy()
+    # dfb_orig_new = dfb_orig.copy()
+    # dfs_cut_new = dfs_cut.copy()
+    # dfb_cut_new = dfb_cut.copy()
+
     difference_s = pd.concat([dfs_orig[cuts1], dfs_cut[cuts1]]).drop_duplicates(keep=False)
     non_log_x = ['cosineneg', 'cosinepos', 'cosinetopo',  'mass', 'pT', 'rapidity',
      'phi', 'eta', 'x', 'y','z', 'px', 'py', 'pz', 'l', 'ldl']
@@ -436,28 +434,28 @@ def variables_distribution(dfs_orig, dfb_orig, dfs_cut, dfb_cut, cuts_plot, pdf_
 
 
     for feat in new_log_x:
-        hist_variables(dfs_orig, dfb_orig, dfs_cut, dfb_cut, difference_s, feat, pdf_cuts)
+        hist_variables(sample, dfs_orig, dfb_orig, dfs_cut, dfb_cut, difference_s, feat, pdf_cuts)
 
     pdf_cuts.close()
 
-variables_distribution(dfs_orig_train, dfb_orig_train, dfs_cut_train, dfb_cut_train, cuts1, pdf_cuts_train)
-variables_distribution(dfs_orig_test, dfb_orig_test, dfs_cut_test, dfb_cut_test, cuts1, pdf_cuts_test)
-variables_distribution(dfs_orig_d, dfb_orig_d, dfs_cut_d, dfb_cut_d, cuts1, pdf_cuts_deploy)
+variables_distribution(' train', dfs_orig_train, dfb_orig_train, dfs_cut_train, dfb_cut_train, cuts1, pdf_cuts_train)
+variables_distribution(' test', dfs_orig_test, dfb_orig_test, dfs_cut_test, dfb_cut_test, cuts1, pdf_cuts_test)
+variables_distribution(' deploy', dfs_orig_d, dfb_orig_d, dfs_cut_d, dfb_cut_d, cuts1, pdf_cuts_deploy)
 
 
 def difference_df(df_orig, df_cut, cut):
     return pd.concat([df_orig[cut], df_cut[cut]]).drop_duplicates(keep=False)
 
 
-# rapidity_range_unclean = [-1.5, 1.5]
-# rapidity_range_clean = [-0.5, 5]
-# pT_range = [-0.5, 3]
-#
-# pT_vs_rapidity(dfs_orig_train, dfs_cut_train, difference_df(dfs_orig_train, dfs_cut_train, cuts1),
-#  1, rapidity_range_clean, pT_range, output_path, ' train')
-#
-# pT_vs_rapidity(dfs_orig_test, dfs_cut_test, difference_df(dfs_orig_test, dfs_cut_test, cuts1),
-#  1, rapidity_range_clean, pT_range, output_path, ' test')
-#
-# pT_vs_rapidity(dfs_orig_d, dfs_cut_d, difference_df(dfs_orig_d, dfs_cut_d, cuts1),
-#   1, rapidity_range_clean, pT_range, output_path, ' deploy')
+rapidity_range_unclean = [-1.5, 1.5]
+rapidity_range_clean = [-0.5, 5]
+pT_range = [-0.5, 3]
+
+pT_vs_rapidity(dfs_orig_train, dfs_cut_train, difference_df(dfs_orig_train, dfs_cut_train, cuts1),
+ 1, rapidity_range_clean, pT_range, output_path, ' train')
+
+pT_vs_rapidity(dfs_orig_test, dfs_cut_test, difference_df(dfs_orig_test, dfs_cut_test, cuts1),
+ 1, rapidity_range_clean, pT_range, output_path, ' test')
+
+pT_vs_rapidity(dfs_orig_d, dfs_cut_d, difference_df(dfs_orig_d, dfs_cut_d, cuts1),
+  1, rapidity_range_clean, pT_range, output_path, ' deploy')
